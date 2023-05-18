9d2a0c9dc9785e9c97899db566ef959e022952f7
Jobson-Pereira-Vicente <jobson.pereira@hotmail.com>
2023-05-18 23:31:50 +0000

    arquivo gasolina.py aletrado em develop

--- gasolina.py
+++ gasolina.py
@@ -15,12 +15,13 @@ gasolina_df
 import seaborn as sns
 
 
-grafico = sns.barplot(data=gasolina_df, x="dia", y="venda", ci=None, palette="pastel")
+grafico = sns.lineplot(data=gasolina_df, x="dia", y="venda", ci=None, color="red")
+
 for i in grafico.patches:
   grafico.annotate(i.get_height(),
-                (i.get_x() +i.get_width() /2, i.get_height()),
+                (i.get_x() +i.get_width() /1, i.get_height()),
                 ha='center')
-grafico.set(title='Preço Médio da Gasolina na Cidade de Sâo Paulo', xlabel='Dia - Julho 2021', ylabel='Valor Médio')
 
+grafico.set(title='Preço Médio da Gasolina na Cidade de Sâo Paulo', xlabel='Dia - 07/2021', ylabel='Valor Médio')
 
 grafico.get_figure().savefig('projeto/gasolina.png')
